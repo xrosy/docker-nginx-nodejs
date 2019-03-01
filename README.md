dc11029e88ff02945d2b6c09838f597d730d2ff6
za-chenzhenyuan <jason@chenzhenyuan.com>
2019-03-01 16:06:30 +0800

    add: .gitignore file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-README.md
+
+
+## 使用说明：
+
+首先在命令行中运行一下命令：
+
+``` shell
+chmod u+x xrosy
+
+chmod g+x xrosy
+```
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-
 #  docker-compose -f "docker-compose.yaml" up -d --build
-
 #  docker-compose -f "docker-compose.yaml" down
+
+echo $0
